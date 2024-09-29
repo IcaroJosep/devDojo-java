34323ce70633d1c51061e70bf75e6d4d86b4dfd5
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-09-29 18:43:14 -0300

    .

---
+++ Texte/src/Carro.java
@@ -0,0 +1,9 @@
+
+public class Carro {
+	String nome;
+	
+	public Carro(String n) {
+		this.nome=n;
+	}
+	
+}
---
+++ Texte/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Carro c1=new Carro("gol");
+		Carro c2=new Carro("fiat");
+		Carro c0=new Carro("golf");
+		
+		Pessoa p =new Pessoa("icaro");
+		
+		p.addCarros(c1,c2,c0);
+		
+		p.imp();
+		
+		p.addCarros(c1,c2,c0);
+		
+		p.imp();
+	
+		
+	}
+
+}
