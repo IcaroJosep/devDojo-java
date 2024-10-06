c5672fa8d3aed09b4591f317f5a99599b025c976
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-10-06 20:42:39 -0300

    .

--- 183 - Generics pt 01 - Introdução/src/Comsumidor.java
+++ 183 - Generics pt 01 - Introdução/src/Comsumidor.java
@@ -1,6 +1,7 @@
 
 public class Comsumidor {
 	String nome;
+	
 	Comsumidor(String n){
 		this.nome=n;
 	}
--- 183 - Generics pt 01 - Introdução/src/Main.java
+++ 183 - Generics pt 01 - Introdução/src/Main.java
@@ -5,12 +5,17 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
+		
+//como era feito nas versoes antes do generics	
 		List lista = new ArrayList();
 		
 		lista.add("midiriya");
 		lista.add(123l);
 		lista.add(new Comsumidor("goku"));
 		
+		/*como a lista aceita qualquer tipo
+		 *seria nescessario fazer a verificasao de tipo 
+		 *em iteraçoes.*/
 		for(Object o:lista) {
 			if(o instanceof String) {
 				System.out.println(o);
@@ -19,14 +24,63 @@ public class Main {
 				System.out.println(o);
 			}
 			if(o instanceof Comsumidor) {
-				System.out.println( ((Comsumidor) o).nome);
-			}
+				System.out.println(((Comsumidor) o).nome);
+			}	
 		}
-		
+
+															System.out.println("\n==============\n");
+															
+//como é feito com generics : 
+		/*a generics foi criado devido a necesidade de definir o tipo da 
+		 *lista e manter a compatibilidade com as versoas anteriores
+		 *
+		 *a definiçao de tipo para lista é feita somente em tempo de compilaçao
+		 *ou seja depos de compilado o codigo nao tem tipo*/
+		List<String> lista2=new ArrayList<>();
 	
+		lista2.add("midiriya");
+		//lista2.add(123l);					 //nao aceita pos lista2 so aceitara strings
+		//lista2.add(new Comsumidor("goku"));//nao aceita pos lista2 so aceitara strings
 		
+		for(String o:lista2) {
+			System.out.println(o);
+		}
+																	System.out.println("\n==========\nintroducao forcada de um obj atraves do metodo:\n");
+		add(lista2,new Comsumidor("icaro"));/*atraves dometodo add estamos add um obj
+											assim demostrando a aceitando todo tipo de elemento
+											pos o metodo chamadado nao trata a lista com generics*/
+												
+	/*	for(String o:lista2) {			//erro pos apos o metodo add 
+			System.out.println(o);		//nem todos os elementos da lista sao Strings
+		}
+	*/
 		
-		
+		//iteracao sem generics sobre a lista generics
+		for(Object o:lista2) {
+			if(o instanceof String) {
+				System.out.println(o);
+			}
+			if(o instanceof Comsumidor) {
+				System.out.println(o);
+			}	
+		}
+	
+	
+	
+	}
+	
+	/*este metodo trata a lista sem generics assim posibilitando
+	 *a inclusao de qualquer tipo dentro da lista mesmo
+	 *q ela tenha sua instancia como generics 
+	 *obs: mertodo criado apenas para demostra q 
+	 *o generics apenas funciona em tempo de compilacao para inpedir a inclusao de 
+	 *qualquer tipo . mas q na memoria tanto a lista com generics quanto a sem 
+	 *sao tratadas da mesma forma */
+	private static void add(List l,Comsumidor c) {
+		l.add(c);
 		
 	}
+	
+	
+
 }
