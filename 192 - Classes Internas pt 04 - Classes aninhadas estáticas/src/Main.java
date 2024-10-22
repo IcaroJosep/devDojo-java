4ed6b4ee424050dfe318ef42c6f3675c99d50fc7
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-10-22 17:40:01 -0300

    .

---
+++ 192 - Classes Internas pt 04 - Classes aninhadas estáticas/src/Main.java
@@ -0,0 +1,35 @@
+
+
+
+public class Main {
+	private String name="icaro";
+	
+	static class Nestd{
+		void print() {
+			System.out.println(new Main().name);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Nestd nestd = new Nestd();
+		nestd.print();
+		/*ou*/
+		new Nestd().print();
+		
+		
+		
+		
+		/*firula minha*/
+		/*ou anonima e estatica*/
+		new Nestd() {
+			@Override
+			void print() {
+			System.out.println("cuss");
+			}
+		}.print();
+		
+	}
+
+}
