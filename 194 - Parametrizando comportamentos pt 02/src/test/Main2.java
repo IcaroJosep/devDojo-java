14fcc3376d707c31b98e789de2daf55b7f3376d7
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-10-23 20:49:10 -0300

    .

--- 194 - Parametrizando comportamentos pt 02/src/test/Main2.java
+++ 194 - Parametrizando comportamentos pt 02/src/test/Main2.java
@@ -10,13 +10,15 @@ public class Main2 {
 	public static void main(String[] args) {
 	
 		List<Integer> nums=List.of(1,2,3,4,5,6,7,8,9);
+											
+											/*nao emporta o nome , pos é uma referencia
+											 * a cada elemento da lista (um for-each com if)*/
+		List<Integer> numsPar= filter(nums, cus->cus%2!=0);
 		
-		List<Integer> numsPar= filter(nums, num->num%2!=0);
-		
-		List<Integer> numsPar2= filter(nums, new Predicate<>() {
+		List<Integer> numsPar2= filter(nums, new Predicate<Integer>() {
 	            @Override
-	            public boolean test(Integer num) {
-	                return num % 2 == 0;
+	            public boolean test(Integer cus) {
+	                return cus % 2 == 0;
 	            }
 	      });
 		
