61ba56b9a863a5be6f3e8294a1f18e77e8126759
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2024-10-25 23:12:06 -0300

    .

---
+++ 195 - Lambdas pt 01 - Predicate/src/test/Main.java
@@ -0,0 +1,49 @@
+package test;
+
+import java.util.function.Function;
+
+/*
+ * para as lambidas funcionarem elas nessecitao q a
+ * interface q tu esteja trabalhando seja funcioanal
+ * 
+ * INTERFACE FUNCIONAL?:
+ * interface q comtem apenas um metodo abstrato 
+ * 
+ * EX:
+ * interface criada e utilizada na ultima aula.
+ * public interface carPredicate{
+ * 		boolean test(car car);
+ * }
+ * 
+ * regras:
+ * 1.pode comter varios metodos; porem apenas um abstarato
+ * */
+public class Main {
+	interface Soma{
+			int adicao(int a);
+		}
+	
+	public static void main(String[] args) {
+		
+		int a = 5;
+	
+	//lambda
+		Soma soma= (e)->{return e*2;};
+		int b =soma.adicao(a);
+		
+		System.out.println(b);
+     
+	//funcao anonima
+        int c = new Soma() {
+			
+			@Override
+			public int adicao(int a) {
+				
+				return a*2;
+			}
+		}.adicao(a);
+		
+		System.out.println(c);
+	}
+
+}
