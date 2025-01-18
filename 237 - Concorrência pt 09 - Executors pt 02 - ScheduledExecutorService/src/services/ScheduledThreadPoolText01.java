4a56be86b147dcd955846810a3a91b82ec4dd05d
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2025-01-18 07:52:03 -0300

    .

--- 237 - Concorrência pt 09 - Executors pt 02 - ScheduledExecutorService/src/services/ScheduledThreadPoolText01.java
+++ 237 - Concorrência pt 09 - Executors pt 02 - ScheduledExecutorService/src/services/ScheduledThreadPoolText01.java
@@ -10,18 +10,25 @@ import java.util.concurrent.TimeUnit;
 
 public class  ScheduledThreadPoolText01 {
 	public static final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
-
+	
+		/*metodo*/
 	public static void beeper() {
+		
+		/*runnable que bipa o horario local*/
 		Runnable r=()->{
 			System.out.println(LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss"))+" beep ");
 		};
+		
 		//executor.schedule(r,5,TimeUnit.SECONDS);
 		//executor.scheduleWithFixedDelay(r, 1, 3, TimeUnit.SECONDS);
+		
+			/*executor q chama o runnable inblementado a uma variavel q é chamada para marcar o fim da execuçao*/
 		ScheduledFuture<?> schundeledFuture =executor.scheduleWithFixedDelay(r, 1, 3, TimeUnit.SECONDS);
+			/*executor q cansela a execuçao de ScheduledFuture(primeiro executor e depois e auto-desliga)*/
 		executor.schedule(()->{
 			System.out.println("fim");
-			schundeledFuture.cancel(false);
-			executor.shutdown();
+			schundeledFuture.cancel(false);//cancela
+			executor.shutdown();//desliga
 		},15,TimeUnit.SECONDS);
 	}
 }
