85c6d2df44eb2eb9b1370408d4c00d15b1bf6b61
icaro josep <147887309+IcaroJosep@users.noreply.github.com>
2025-04-27 15:35:58 -0300

    ,

---
+++ 265 - JDBC pt 14 - ResultSet TYPE SCROLL INSENSITIVE/src/main/java/teste/ConnectionFactoryTest.java
@@ -0,0 +1,39 @@
+package teste;
+
+import dominio.Producer;
+import services.ProducerServices;
+/*nesta aula a a esploracao do TYPE SCROLL INSENSITIVE
+ * com leituras de cima pra baixo ,baixo pra cima e posiçao expecitca etc.. 
+ * 
+ * #atribuço de novo metodo em ProducerRepository "shoTipeScrollWorcking"
+ * 
+ * 
+ * */
+
+
+
+public class ConnectionFactoryTest {
+	public static void main(String[] args) {	
+			
+		/*Producer a = Producer.builder().name("kome ku").build();
+		 
+		 	ProducerServices.save(a);
+		 */
+		
+		//ProducerServices.delet(5);
+		
+		//ProducerServices.update(28,"machado");
+		
+		//System.out.println(ProducerServices.findAll());
+		
+		//System.out.println(ProducerServices.findByName("k"));
+		
+		//ProducerServices.showProducerMetadata();
+		
+		//ProducerServices.showDriverMetadata();	
+		
+	    ProducerServices.showTipeScrollWorcking();
+		
+		System.out.println("fim");
+	}
+}
